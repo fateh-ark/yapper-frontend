4c24c2df0e565e566098e912c618e850463590a1
M. Fatih A <m.akbar@student.fontys.nl>
2025-02-18 17:13:50 +0100

    Added initial navigation in homescreen and Post CTA.

---
+++ src/components/card/PostCard.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { PhPaperPlaneRight } from '@phosphor-icons/vue'
+import { CircleProgressBar } from 'circle-progress.vue'
+
+const props = defineProps({
+  maxCharacters: {
+    type: Number,
+    default: 250,
+  },
+})
+
+const content = ref('')
+const charCount = computed(() => {
+  return content.value.length
+})
+</script>
+
+<template>
+  <div class="flex flex-row gap-3">
+    <img src="" alt="pfp" class="w-[40px] h-[40px] bg-stone-500 rounded-full" />
+    <div class="grow">
+      <textarea
+        id="content"
+        rows="3"
+        v-model="content"
+        placeholder="What do you want to Yap about?"
+        class="w-full text-xl outline-none resize-none field-sizing-content"
+      ></textarea>
+      <hr class="border-stone-700 my-3" />
+      <div class="w-full flex flex-row gap-3 items-center">
+        <p
+          class="grow text-right"
+          :class="[charCount <= props.maxCharacters ? 'text-stone-500 ' : 'text-red-500 font-bold']"
+        >
+          {{ charCount }}/{{ props.maxCharacters }}
+        </p>
+        <CircleProgressBar
+          :value="charCount"
+          :max="props.maxCharacters"
+          :size="26"
+          strokeWidth="15px"
+          :rounded="true"
+          colorFilled="#fb2c36"
+          colorUnfilled="#f0b100"
+          colorBack="#44403b"
+          :startAngle="0"
+        />
+        <div class="h-6 w-px border-l border-stone-500"></div>
+        <button
+          class="bg-white hover:bg-amber-100 cursor-pointer text-black rounded-full font-semibold px-4 py-1 flex flex-row gap-1 items-center"
+        >
+          <PhPaperPlaneRight weight="fill" />
+          Post
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,28 @@
+<script setup>
+import PostCard from '../components/card/PostCard.vue'
+import { ref } from 'vue'
+
+const isForYou = ref('true')
+</script>
+
 <template>
-  <main>
-    <h1 class="text-5xl font-bold underline text-white">Hello world!</h1>
+  <main class="md:w-3xl sm-2xl w-full h-full min-h-screen border-x border-stone-700 mx-auto">
+    <div class="sticky w-full border-b border-stone-700 flex flex-row text-lg font-semibold">
+      <button
+        class="grow px-2 pt-3 hover:bg-white/10 cursor-pointer"
+        :class="{ 'text-stone-500 pb-3': !isForYou }"
+      >
+        Yaps for you
+        <div v-if="isForYou" class="h-1 w-1/2 mx-auto mt-2 bg-yellow-500 rounded-full"></div>
+      </button>
+      <button
+        class="grow px-2 pt-3 hover:bg-white/10 cursor-pointer"
+        :class="{ 'text-stone-500 pb-3': isForYou }"
+      >
+        Followed Yappers
+        <div v-if="!isForYou" class="h-1 w-1/2 mx-auto mt-2 bg-yellow-500 rounded-full"></div>
+      </button>
+    </div>
+    <PostCard class="w-full border-b border-stone-700 p-4" />
   </main>
 </template>
